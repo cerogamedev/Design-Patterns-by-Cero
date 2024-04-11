b917625b5fefaafb8c9dc5524d55db047de1e555
Ceren <ingortigno@gmail.com>
2024-04-12 02:06:41 +0300

    Singleton and Observer

---
+++ Design Patters/Assets/Observer/Observer.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Observer : MonoBehaviour
+{
+    public abstract void OnNotify(NotificationType notificationType);
+
+}
